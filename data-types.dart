bdf15f596e034e5e236f7318da96bd100403811f
avict1809 <avict1809@proton.me>
2025-06-12 11:26:04 +0300

    lesson on data types

---
+++ data-types.dart
@@ -0,0 +1,26 @@
+/*
+  Data types
+  Numbers, Strings, Boolean, Lists, Maps, Dynamic
+*/
+
+void main(){
+  //Strings
+  String firstName = "Avict";
+  print("String: $firstName");
+
+  //Integers
+  int myNum = 45;
+  print("Integer: $myNum");
+
+  //Doubles
+  double myDouble = 45.5;
+  print("Double: $myDouble");
+
+  //Boolean
+  bool myBool = true;
+  print("Boolean: $myBool");
+
+  //Dynamic
+  dynamic fullName = "John Doe";
+  print("Dynamic: $fullName");
+}
