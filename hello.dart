0e50c3e3686e67e4084e2e1172a2815fd3f23368
avict1809 <avict1809@proton.me>
2025-06-12 10:53:48 +0300

    Main function and print() function

---
+++ hello.dart
@@ -0,0 +1,9 @@
+/*
+  Every app requires the top-level main() function, where execution starts. 
+  Functions that don't explicitly return a value have the void return type. 
+  To display text on the console, use the top-level print() function:
+*/
+
+void main() {
+  print('Hello, World!');
+}
